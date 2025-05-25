a0d7595f4fe69e38eddbe8ab2883d7b74417161a
Prodkt <technology@prodkt.cloud>
2025-05-24 22:06:01 -0600

    bring in spline

--- src/components/LogoFive.tsx
+++ src/components/LogoFive.tsx
@@ -5,7 +5,7 @@ export default function LogoFive() {
     <section className="h-dvh w-dvw max-h-dvh max-w-dvw bg-[rgba(0,0,0,0.1)]">
 
       <Spline
-        scene="https://prod.spline.design/yqeWTxsiLPbNFWQu/scene.splinecode" 
+        scene="spline/scene5.splinecode" 
         width={1920}
         height={1080}
         className={'size-full inset-0 m-0 max-w-dvw max-h-dvh'}
--- src/components/LogoOne.tsx
+++ src/components/LogoOne.tsx
@@ -4,7 +4,7 @@ export default function LogoOne() {
   return (
     <section className="h-dvh w-dvw max-h-dvh max-w-dvw bg-[rgba(0,0,0,0.1)]">
       <Spline
-        scene="https://prod.spline.design/quod7ENN4Vv3d01l/scene.splinecode" 
+        scene="spline/scene.splinecode" 
         width={1920}
         height={1080}
         className={'size-full inset-0 m-0 max-w-dvw max-h-dvh'}
